244898fa625f457504842238788d529e66bd280e
Justin Frahm <justinmfrahm@gmail.com>
2020-05-09 13:43:02 -0600

    Add some useful output, correct filename check

--- launcher/container/entrypoint.sh
+++ launcher/container/entrypoint.sh
@@ -9,12 +9,13 @@ chmod 400 ~/.ssh/id_rsa.pem
 TARGET_SERVER_PATH="infra-live/${INFRA_LIVE_PATH}/${SERVER_NAME}"
 echo "Cloning '$INFRA_LIVE_CLONE_URL'"
 git clone -q "$INFRA_LIVE_CLONE_URL" infra-live
-if [ ! -f "${TARGET_SERVER_PATH}/terraform.hcl" ]; then
-  echo "Configuration file not found at ${TARGET_SERVER_PATH}/terraform.hcl"
+if [ ! -f "${TARGET_SERVER_PATH}/terragrunt.hcl" ]; then
+  echo "Configuration file not found at ${TARGET_SERVER_PATH}/terragrunt.hcl"
   echo "Generating from template..."
   mkdir -p "${TARGET_SERVER_PATH}"
   sed "s/__SERVER_NAME__/${SERVER_NAME}/g" "infra-live/templates/${ENVIRONMENT}/minecraft/terragrunt.hcl" >"$TARGET_SERVER_PATH/terragrunt.hcl"
 fi
 echo "Launching server at $TARGET_SERVER_PATH"
 cd "${TARGET_SERVER_PATH}"
+cat terragrunt.hcl
 terragrunt apply -input=false --auto-approve
